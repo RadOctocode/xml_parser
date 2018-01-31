51fbe377c7c228822405df3e3517b82ac166dcc0
RadOctocode <mtan2198@gmail.com>
2018-01-31 11:41:10 -0500

    parser now holds timestamp and ip, driver can now login to splunk

--- xml_parser.py
+++ xml_parser.py
@@ -8,7 +8,6 @@ mydoc = minidom.parse('example.xml')
 
 ip_add = mydoc.getElementsByTagName('IP_Address')
 time_stamp = mydoc.getElementsByTagName('TimeStamp')
-print('IP:')  
 print(ip_add[0].firstChild.data)
 
 split_time=(time_stamp[0].firstChild.data).replace("T", " ")
@@ -20,30 +19,31 @@ print(d2-datetime.timedelta(hours=5))#prints out time stamp 5 hours before
 
 #driver sample code
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+driver.get("https://binghamton.splunkcloud.com/en-US/account/login?return_to=%2Fen-US%2F")
+username = driver.find_element_by_id("username")
+password = driver.find_element_by_id("password")
+
+
+username.send_keys("username")#put in username
+password.send_keys("password")#put in password
+
+
+login_attempt = driver.find_element_by_xpath("//*[@type='submit']")
+login_attempt.submit()
+
+
+
+
+
+
+
+
+
+
+#driver.close()
 #end of driver sample code
 
-#seprarte at T and find the last letter and adjust
-#+- 2 hours
-#all item attributes
-#print('\nAll attributes:')  
-#for elem in items:  
-#        print(elem.attributes['name'].value)
+#resources               
+#http://www.thetaranights.com/login-to-a-website-using-selenium-python-python-selenium-example/
+#https://pythonspot.com/selenium-click-button/
 
-# one specific item's data
-#print('\nItem #2 data:')  
-#print(items[1].firstChild.data)  
-#print(items[1].childNodes[0].data)
-
-# all items data
-#print('\nAll item data:')  
-#for elem in items:  
-#	print(elem.firstChild.data)
-                
